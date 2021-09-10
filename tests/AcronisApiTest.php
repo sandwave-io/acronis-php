cefbc2b21138b31673ae5a554ea7d52e1ab8a2ac
Sander van Stokkom <sander@argeweb.nl>
2021-09-10 12:26:45 +0200

    Fixed merge conflicts

--- tests/AcronisApiTest.php
+++ tests/AcronisApiTest.php
@@ -1,10 +1,6 @@
 <?php
 
-<<<<<<< HEAD
-declare(strict_types=1);
-=======
 declare(strict_types = 1);
->>>>>>> d87781798975b938cd701245b3e7e5edb382a8e3
 
 namespace SandwaveIo\Acronis\Tests;
 
@@ -13,11 +9,8 @@ use PHPUnit\Framework\TestCase;
 use SandwaveIo\Acronis\AcronisApi;
 use SandwaveIo\Acronis\Client\RestClient;
 use SandwaveIo\Acronis\Client\TenantClient;
-<<<<<<< HEAD
 use SandwaveIo\Acronis\Client\UserClient;
 use SandwaveIo\Acronis\Client\UsageClient;
-=======
->>>>>>> d87781798975b938cd701245b3e7e5edb382a8e3
 use SandwaveIo\Acronis\RestClientFactory;
 
 class AcronisApiTest extends TestCase
@@ -34,7 +27,6 @@ class AcronisApiTest extends TestCase
         $acronisApi = new AcronisApi($restClient);
 
         $this->assertInstanceOf(AcronisApi::class, $acronisApi, 'The Acronis API could not be instantiated.');
-<<<<<<< HEAD
         $this->assertInstanceOf(
             TenantClient::class,
             $acronisApi->getTenantClient(),
@@ -50,8 +42,5 @@ class AcronisApiTest extends TestCase
             $acronisApi->getUsageClient(),
             'The Usage Client could not be instantiated.'
         );
-=======
-        $this->assertInstanceOf(TenantClient::class, $acronisApi->getTenantClient(), 'The Tenants Client could not be instantiated.');
->>>>>>> d87781798975b938cd701245b3e7e5edb382a8e3
     }
 }
