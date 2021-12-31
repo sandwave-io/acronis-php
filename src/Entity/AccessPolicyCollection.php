e04e3d480881734d04f19f0a792303795254d61e
Glenn Blom <glennblom@gmail.com>
2021-12-31 09:30:46 +0100

    Add user access policies (#13)

---
+++ src/Entity/AccessPolicyCollection.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace SandwaveIo\Acronis\Entity;
+
+use JMS\Serializer\Annotation as Serializer;
+
+class AccessPolicyCollection
+{
+    /**
+     * @var AccessPolicy[]
+     * @Serializer\Type("array<SandwaveIo\Acronis\Entity\AccessPolicy>")
+     * @Serializer\SerializedName("items")
+     * @Serializer\Groups({"update_data"})
+     */
+    private $items;
+
+    /**
+     * @param AccessPolicy[] $items
+     */
+    public function __construct(array $items)
+    {
+        $this->setItems($items);
+    }
+
+    /**
+     * @return AccessPolicy[]
+     */
+    public function getItems(): array
+    {
+        return $this->items;
+    }
+
+    /**
+     * @param AccessPolicy[] $items
+     *
+     * @return AccessPolicyCollection
+     */
+    public function setItems(array $items): AccessPolicyCollection
+    {
+        $this->items = $items;
+        return $this;
+    }
+}
